cecf6faa88cd39d392a9ef14c445d4c793f75f6a
yangchaolin <289459620@qq.com>
2019-03-06 18:26:55 +0800

    项目搭建

---
+++ severAndClient/capabilityPreview/study/src/components/Home.vue
@@ -0,0 +1,38 @@
+<template>
+    <div  style="height: 100%;width:100%">
+        <div class="container">
+            <div class="PC"></div>
+            <div class="小程序"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+	export default {
+		name: "home",
+		components: {},
+		mounted () {
+			this.init ();
+		},
+		data () {
+			return {}
+		},
+		methods: {
+			init () {
+			}
+		}
+	}
+</script>
+
+<style scoped>
+    .container{
+        background-color: #42b983;
+        width: 800px;
+        height: 600px;
+        margin: 0 auto;
+        position: relative;
+    }
+    .PC{
+    
+    }
+</style>
